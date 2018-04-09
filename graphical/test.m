97c92c6b6fa190e95ad2320fe7755239bad0e88b
Kim Young <youngkim0911@163.com>
2018-04-09 16:22:43 +0800

    Graphical Display

---
+++ graphical/test.m
@@ -0,0 +1,8 @@
+for i = 0 :0.1 :2.5
+    R = [ 1/2 -sqrt(3)/2 0; 
+        sqrt(3)/2*cos(i) 1/2*cos(i) -sin(i);
+        sqrt(3)/2*sin(i) 1/2*sin(i) cos(i)];
+    t = [ 1-0.5*i; 1+0.5*i; 1-0.5*i];
+    graphical(R,t)
+end
+
