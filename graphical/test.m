c2aea4af45c5a850ea447f1e6897decf93b13987
Ben Lin <522181941@qq.com>
2018-04-11 12:25:51 +0800

    Add Graphical display PSM MTM

--- graphical/test.m
+++ graphical/test.m
@@ -1,4 +1,4 @@
-for i = 0 :0.1 :2.5
+for i = 0 :0.1 :5
     R = [ 1/2 -sqrt(3)/2 0; 
         sqrt(3)/2*cos(i) 1/2*cos(i) -sin(i);
         sqrt(3)/2*sin(i) 1/2*sin(i) cos(i)];
--- kinetic_control/Control System/PSM_Direct_Control.m
+++ kinetic_control/Control System/PSM_Direct_Control.m
@@ -6,28 +6,32 @@ duration = 5;
 PSM_Model = PSM_DH_Model();
 [x0,J0] = FK_Jacob_Geometry(PSM_q0,PSM_Model.DH, PSM_Model.tip, PSM_Model.method);
 
-% for t = time_delta:time_delta:duration
-%     qt = PSM_q(:,end);
-%     [xt,Jt] = FK_Jacob_Geometry(qt,PSM_Model.DH, PSM_Model.tip, PSM_Model.method);
-%     [xd_t, vd_t] = Design_Trajectory(t,x0);
-%     [xe_t, delta_theta] = T_Error(xt,xd_t);
-%     qdot_t = Inv_Jacob_Control(xe_t, vd_t, Jt, lambda);
-%     PSM_q = [PSM_q, qt+qdot_t*time_delta];
-%     PSM_graphical(xt(1:3,1:3), xt(1:3,4));
-% end
+d_size = size(psm_x_dsr);
+for i = 1:d_size(3)
+    qt = PSM_q(:,end);
+    [xt,Jt] = FK_Jacob_Geometry(qt,PSM_Model.DH, PSM_Model.tip, PSM_Model.method);
+    xd_t = psm_x_dsr(:,:,i);
+    [xe_t, delta_theta] = T_Error(xt,xd_t);
+    vd_t = psm_xdot_dsr(:,i);
+    qdot_t = Inv_Jacob_Control(xe_t, vd_t, Jt, lambda);
+    PSM_q = [PSM_q, qt+qdot_t*time_delta];
+    if(mod(i,50) == 0)
+        PSM_graphical(xt(1:3,1:3), xt(1:3,4),i);
+    end
+end
 
 % for t = time_delta:time_delta:duration
 %     [xd_t, vd_t] = Design_Trajectory(t,x0);
 %     graphical(xd_t(1:3,1:3), xd_t(1:3,4));
 % end
 
-% d_size = size(psm_x_dsr);
-% for i = 1:d_size(3)
-%     if(mod(i,50) == 0)
-%         graphical(psm_x_dsr(1:3,1:3,i), psm_x_dsr(1:3,4,i),i);
-%         %pause(0.5);
-%     end
-% end
+d_size = size(psm_x_dsr);
+for i = 1:d_size(3)
+    if(mod(i,50) == 0)
+        PSM_graphical(psm_x_dsr(1:3,1:3,i), psm_x_dsr(1:3,4,i),i);
+        %pause(0.5);
+    end
+end
 
 % d_size = size(mtm_x);
 % for i = 1:d_size(3)
